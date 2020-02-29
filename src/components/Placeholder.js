9a9d0ce6044847488e0a6e4081a1ad57cdd84ebf
Manan Patel <patel.mana@husky.neu.edu>
2020-02-29 12:43:48 -0500

    added structure, files, routing

---
+++ src/components/Placeholder.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+function Placeholder() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Agora
+                </p>
+            </header>
+        </div>
+    );
+}
+export default Placeholder;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
