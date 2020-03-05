fc9efe0f011c7aed0a5ac82bf78cb341d45c22a8
Manan Patel <patel.mana@husky.neu.edu>
2020-03-05 18:42:06 -0500

    Added create upcoming hosted event buttons

--- src/components/AgoraNavbar.js
+++ src/components/AgoraNavbar.js
@@ -11,6 +11,9 @@ class AgoraNavbar extends React.Component {
             <nav className="navbar navbar-dark bg-dark">
                 <a className="navbar-brand" href="#"><h1>Agora</h1></a>
                 <form className="form-inline form-group row ml-4 mr-3">
+                    <button className="btn btn-outline-info mr-sm-4 my-2 my-sm-0" type="submit">Create Event</button>
+                    <button className="btn btn-outline-info mr-sm-4 my-2 my-sm-0" type="submit">Upcoming</button>
+                    <button className="btn btn-outline-info mr-sm-4 my-2 my-sm-0" type="submit">Hosted</button>
                     <input className="form-control mr-sm-4" type="search" placeholder="Search Event"
                            aria-label="Search"/>
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
