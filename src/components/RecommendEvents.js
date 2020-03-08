848efb5221d8d6d25cb35b7ea08574ea964b90f0
Manan Patel <patel.mana@husky.neu.edu>
2020-03-08 12:35:47 -0400

    Resolved Merge Conflicts and using SearchService

--- src/components/RecommendEvents.js
+++ src/components/RecommendEvents.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import SearchCard from './SearchCard';
-
+import SearchService from "../services/SearchService";
 
 
 const recommendEvents = () => (
+    <div>
+        <h3 style={{textAlign: "left", marginBottom: 85}}><i>Recommendations</i></h3>
+        <div className="pre-scrollable bg-light">
+            {SearchService.getInstance().getRecommendedCards().map((card) => {
+                return <div className="m-5">
+                    <SearchCard cardProps={card}/>
+                </div>
+            })}
+        </div>
+    </div>
 
-        <div>
-            <h3 style={{textAlign:"left", marginBottom: 85}}><i>Recommendations</i></h3>
-            <div className="pre-scrollable bg-light">
-            <div className="m-3">
-                    <SearchCard />
-                </div>
-                <div className="m-3">
-                    <SearchCard />
-                </div>
-                <div className="m-3">
-                    <SearchCard />
-                </div><div className="m-3">
-                <SearchCard />
-            </div>
-          
-        </div>
-        </div>
-        
-        
-    
-    
-        
-    
+
 );
 
 
