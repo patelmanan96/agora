7c3e1c1eea87ce809154fc292665d06c28f5e428
Manan Patel <patel.mana@husky.neu.edu>
2020-04-11 17:06:44 -0400

    sorting on search page

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -16,11 +16,12 @@ class FilterBar extends React.Component {
                     <option value="3">Three</option>
                 </select>
                 <label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">Sort By : </label>
-                <select className="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-                    <option selected>Choose...</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                <select value={this.props.sortValue} onChange={(event) => this.props.changeSort(event.target.value)}
+                        className="custom-select my-1 mr-sm-2"
+                        id="inlineFormCustomSelectPref">
+                    <option value="sooner">Sooner</option>
+                    <option value="later">Later</option>
+                    <option value="title">Title</option>
                 </select>
             </div>
 
--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -9,10 +9,25 @@ class SearchPage extends React.Component {
         this.searchService = SearchService.getInstance();
         this.state = {
             query: this.props.match.params.query,
-            cards: this.chunk(this.searchService.getCards(this.props.match.params.query), 3)
-        }
-
+            cards: this.chunk(this.searchService.getCards(this.props.match.params.query), 3),
+            sort: 'title'
+        };
     }
+
+    changeSort = (sortType) => {
+        this.setState({sort: sortType});
+        let currentCards = this.searchService.getCards(this.props.match.params.query);
+        if (sortType === 'title') {
+            let titleSorted = currentCards.sort((card1, card2) => card1.title.localeCompare(card2.title));
+            this.setState({cards: this.chunk(titleSorted, 3)})
+        } else if (sortType === 'sooner') {
+            let soonerSorted = currentCards.sort((card1, card2) => card1.date < card2.date ? 1 : -1);
+            this.setState({cards: this.chunk(soonerSorted, 3)})
+        } else {
+            let laterSorted = currentCards.sort((card1, card2) => card1.date > card2.date ? 1 : -1);
+            this.setState({cards: this.chunk(laterSorted, 3)})
+        }
+    };
 
     chunk = (arr, size) =>
         Array.from({length: Math.ceil(arr.length / size)}, (v, i) =>
@@ -30,7 +45,9 @@ class SearchPage extends React.Component {
                         </div>
                     </div>
                     <div className="col-sm">
-                        <FilterBar/>
+                        <FilterBar sortValue={this.state.sort}
+                                   changeSort={this.changeSort}
+                        />
                     </div>
                 </div>
 
