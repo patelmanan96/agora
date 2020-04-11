4fca46678c20ddea863005ebedaf0c8f384313ab
Manan Patel <patel.mana@husky.neu.edu>
2020-04-11 15:08:07 -0400

    Added Tag creation and handled all ui fixes

--- src/containers/CreateEvent.js
+++ src/containers/CreateEvent.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import TagList from "../components/TagList";
 
 class CreateEvent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            status: false
+            status: false,
+            tags: []
         }
     }
 
@@ -13,7 +15,25 @@ class CreateEvent extends React.Component {
         this.setState({
             status: true
         })
-    }
+    };
+
+    removeTag = (i) => {
+        const newTags = [...this.state.tags];
+        newTags.splice(i, 1);
+        this.setState({tags: newTags});
+    };
+
+    inputKeyDown = (e) => {
+        const val = e.target.value;
+        if (e.key === 'Enter' && val) {
+            if (this.state.tags.find(tag => tag.toLowerCase() === val.toLowerCase())) {
+                return;
+            }
+            this.setState({tags: [...this.state.tags, val]});
+        } else if (e.key === 'Backspace' && !val) {
+            this.removeTag(this.state.tags.length - 1);
+        }
+    };
 
     render() {
         return (
@@ -33,33 +53,36 @@ class CreateEvent extends React.Component {
 
                         <div className="form-row">
                             <div className="form-group col-md-6">
-                                <label htmlFor="inputEmail4">Event Title</label>
-                                <input type="text" className="form-control" id="inputEmail4"/>
+                                <label htmlFor="inputEmail4">Event Title *</label>
+                                <input type="text" placeholder="Event Title" className="form-control" id="inputEmail4"/>
                             </div>
                             <div className="form-group col-md-6">
-                                <label htmlFor="inputPassword4">Event Location</label>
-                                <input type="text" className="form-control" id="inputPassword4"/>
+                                <label htmlFor="inputPassword4">Event Location *</label>
+                                <input type="text" placeholder="Location of the Event" className="form-control"
+                                       id="inputPassword4"/>
                             </div>
                         </div>
                         <div className="form-row">
                             <div className="form-group col-md-6">
-                                <label htmlFor="inputAddress">Event Date</label>
+                                <label htmlFor="inputAddress">Event Date *</label>
                                 <input type="date" className="form-control" id="inputAddress"
                                        placeholder="1234 Main St"/>
                             </div>
                             <div className="form-group col-md-6">
-                                <label htmlFor="inputPassword4">Organizer Name</label>
-                                <input type="text" className="form-control" id="inputPassword4"/>
+                                <label htmlFor="inputPassword4">Organizer Name *</label>
+                                <input type="text" placeholder="Name of the person organizing event"
+                                       className="form-control" id="inputPassword4"/>
                             </div>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="inputAddress2">Event Summary</label>
+                            <label htmlFor="inputAddress2">Event Summary *</label>
                             <input type="text" className="form-control" id="inputAddress2"
-                                   placeholder="Apartment, studio, or floor"/>
+                                   placeholder="Brief event summary"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleFormControlTextarea1">Event Description</label>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea placeholder="Detiled Event Description" className="form-control"
+                                      id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
 
                         <div className="form-group">
@@ -67,27 +90,13 @@ class CreateEvent extends React.Component {
                             <input type="file" className="form-control" id="inputAddress2"
                                    placeholder="Apartment, studio, or floor"/>
                         </div>
+
                         <div className="form-group">
-                            <label htmlFor="inputAddress2">TAGS</label>
-                            <div className="container form-check">
-
-                                <column col-sm="3"><input type="checkbox"/><label>CAMD</label></column>
-                                <column col-sm="3"><input type="checkbox"/><label>School of Business</label></column>
-                                <column col-sm="3"><input type="checkbox"/><label>Khoury</label></column>
-
-
-                                <column col-sm="true"><input type="checkbox"/><label>College of Engineering</label>
-                                </column>
-                                <column col-sm="true"><input type="checkbox"/><label>Health Sciences</label></column>
-                                <column col-sm="true"><input type="checkbox"/><label>Law</label></column>
-
-
-                                <column col-sm="true"><input type="checkbox"/><label>CPS</label></column>
-                                <column col-sm="true"><input type="checkbox"/><label>College of Science</label></column>
-                                <column col-sm="true"><input type="checkbox"/><label>Social Sciences and
-                                    Humanities</label></column>
-
-                            </div>
+                            <label htmlFor="inputAddress2">Tags</label>
+                            <TagList tags={this.state.tags}
+                                     inputKeyDown={this.inputKeyDown}
+                                     removeTag={this.removeTag}
+                            />
                         </div>
 
                         <button onClick={this.create} type="submit" className="btn btn-primary">Create</button>
